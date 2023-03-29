3c4cd842baa3cfa5617e3f8214376e3f96914a60
Holli Dawson <hldawson42@gmail.com>
2023-03-29 11:50:40 -0700

    completes test

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) +1;
+
+let num3 = 20;
+let num4= 8;
+const mod = num3 % num4; 
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
